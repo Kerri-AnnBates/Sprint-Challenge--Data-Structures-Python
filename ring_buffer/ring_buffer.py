44171842f911fe50fda22e4f032cecb9e1979417
Kerri-Ann <kaybee1jam@yahoo.com>
2020-03-27 15:06:41 -0400

    ring buffer pending..

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,51 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #  When the ring buffer is full and a new element is inserted, the oldest element in the ring buffer is overwritten with the newest element.
+        # [ 'oldest', ... , 'recent' ]
+        # [ head, ... , tail ]
+        # [ 'a', 'b', 'c' ]
+
+        if len(self.storage) is not self.capacity:
+            self.storage.add_to_tail(item)
+
+            # set current to the oldest item
+            self.current = self.storage.head
+            print(self.current.value)
+        else:
+            if self.current.next:  # If not the tail
+                # overwrite the oldest item which is initially the head (current)
+                next_node = self.current.next
+                print("next node:", next_node.value)
+                print("item:", item)
+                self.current.insert_before(item)
+                print("current node after inser_before:", self.current.value)
+                # self.current.delete()
+                # self.storage.delete(self.current)
+                self.current = next_node
+
+            else:
+                next_node = self.storage.head
+
+                self.current.insert_before(item)
+                self.storage.delete(self.current)
+                self.current = next_node
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        if len(self.storage):
+
+            curr = self.storage.head
+
+            while curr:
+                list_buffer_contents.append(curr.value)
+                curr = curr.next
 
         return list_buffer_contents
+
 
 # ----------------Stretch Goal-------------------
 
@@ -30,3 +66,10 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+# Set current to the oldest item
+# if current is not the tail
+# set the next storage item to current
+#  set the prev to the new item
+#  set storage.next to storage.next.next
