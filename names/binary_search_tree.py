5846d3574f32908172936f9b43ef6a0b432e825e
Kerri-Ann <kaybee1jam@yahoo.com>
2020-03-27 15:59:40 -0400

    removed extra files

--- names/binary_search_tree.py
+++ names/binary_search_tree.py
@@ -1,14 +1,8 @@
-from dll_queue import Queue
-from dll_stack import Stack
-
-
 class BinarySearchTree:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-        self.queue = Queue()
-        self.stack = Stack()
 
     # Insert the given value into the tree
     def insert(self, value):
@@ -76,71 +70,3 @@ class BinarySearchTree:
         # 3. Traverse the right subtree
         if self.right:
             self.right.for_each(cb)
-
-    # DAY 2 Project -----------------------
-
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        # Go L -> print N -> Go R
-        # keep going left until can't go any further
-        if node.left:
-            node.left.in_order_print(node.left)
-
-        #  print the node
-        self.queue.enqueue(node.value)
-        # print(self.queue.dequeue())
-        return self.queue.dequeue()
-
-        # Go right until can't go any further
-        if node.right:
-            node.right.in_order_print(node.right)
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-
-    def bft_print(self, node):
-        #  use a queue
-        self.queue.enqueue(node)
-
-        # while queue is not empty
-        while self.queue.len() > 0:
-
-            n = self.queue.dequeue()
-            print(n.value)
-
-            # add children of node to queue
-            if n.left:
-                self.queue.enqueue(n.left)
-
-            if n.right:
-                self.queue.enqueue(n.right)
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
-    def dft_print(self, node):
-        # use a stack
-        self.stack.push(node)
-
-        # while stack is not empty
-        while self.stack.len() > 0:
-            # print node
-            n = self.stack.pop()
-            print(n.value)
-
-            if n.left:
-                self.stack.push(n.left)
-
-            if n.right:
-                self.stack.push(n.right)
-
-    # STRETCH Goals -------------------------
-    # Note: Research may be required
-
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
-
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
